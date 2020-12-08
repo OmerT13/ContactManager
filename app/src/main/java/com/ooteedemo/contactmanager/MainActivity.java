2ff7254d6b8c9755f4f0bed4f17be3ff15243858
OT13 <omertahir13@gmail.com>
2020-12-08 08:21:42 +0800

    Added methods for get all contacts, get one contact, and update contact. Tested with two contacts in the main activity

--- app/src/main/java/com/ooteedemo/contactmanager/MainActivity.java
+++ app/src/main/java/com/ooteedemo/contactmanager/MainActivity.java
@@ -3,6 +3,12 @@ package com.ooteedemo.contactmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ooteedemo.contactmanager.data.DatabaseHandler;
+import com.ooteedemo.contactmanager.model.Contact;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatabaseHandler db = new DatabaseHandler(MainActivity.this);
+
+        Contact jeremy = new Contact();
+        jeremy.setName("Jeremy");
+        jeremy.setPhoneNumber("0134456543");
+//        db.addContact(jeremy);
+
+        Contact jason = new Contact();
+        jason.setName("Jason");
+        jason.setPhoneNumber("0193827384");
+//        db.addContact(jason);
+
+
+        List<Contact> contactList = db.getAllContacts();
+        for (Contact contact: contactList) {
+            Log.d("DBHandler", "getting all contacts: "+contact.getId()+contact.getName());
+        }
+
     }
 }
--- app/src/main/java/com/ooteedemo/contactmanager/data/DatabaseHandler.java
+++ app/src/main/java/com/ooteedemo/contactmanager/data/DatabaseHandler.java
@@ -1,13 +1,21 @@
 package com.ooteedemo.contactmanager.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.ooteedemo.contactmanager.R;
+import com.ooteedemo.contactmanager.model.Contact;
 import com.ooteedemo.contactmanager.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
     public DatabaseHandler(Context context) {
@@ -19,9 +27,9 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
 
         String CREATE_CONTACT_TABLE = "CREATE TABLE " + Util.TABLE_NAME + "(" +
-                Util.KEY_ID + "INTEGER PRIMARY KEY," +
-                Util.KEY_NAME + "TEXT," +
-                Util.KEY_PHONE_NUMBER + "TEXT" +
+                Util.KEY_ID + " INTEGER PRIMARY KEY," +
+                Util.KEY_NAME + " TEXT," +
+                Util.KEY_PHONE_NUMBER + " TEXT" +
                 ")";
 
         db.execSQL(CREATE_CONTACT_TABLE);
@@ -34,5 +42,71 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL(DROP_TABLE, new String[]{Util.DATABASE_NAME});
 
         onCreate(db);
+    }
+    
+//    CRUD
+    public void addContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME,contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER,contact.getPhoneNumber());
+
+        db.insert(Util.TABLE_NAME,null,values);
+        Log.d("DBHandler", "itemAdded "+contact.getPhoneNumber());
+        db.close();
+    }
+
+    public Contact getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Util.TABLE_NAME,
+                new String[] {Util.KEY_ID,Util.KEY_NAME,Util.KEY_PHONE_NUMBER},
+                Util.KEY_ID+"=?", new String[]{String.valueOf(id)},
+                null,null,null);
+
+        if (cursor !=null)
+            cursor.moveToFirst();
+
+        Contact contact = new Contact();
+        contact.setId(Integer.parseInt(cursor.getString(0)));
+        contact.setName(cursor.getString(1));
+        contact.setPhoneNumber(cursor.getString(2));
+        
+        return contact;
+    }
+
+    public List<Contact> getAllContacts() {
+        List contactList = new ArrayList<>();
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        String selectAll = "SELECT * FROM " + Util.TABLE_NAME;
+        Cursor cursor = db.rawQuery(selectAll,null);
+
+//        Loop through the data
+        if (cursor.moveToFirst()) {
+            do {
+                Contact contact = new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+
+                contactList.add(contact);
+
+            } while (cursor.moveToNext());
+        }
+        return contactList;
+    }
+
+    public int updateContact(Contact contact) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(Util.KEY_NAME,contact.getName());
+        values.put(Util.KEY_PHONE_NUMBER,contact.getPhoneNumber());
+
+        return db.update(Util.TABLE_NAME,values,Util.KEY_ID+"=?",
+                new String[]{String.valueOf(contact.getId())});
     }
 }
